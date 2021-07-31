d36e4add56cb4920ebcdd12ae19f73e9dcc47ece
Corey Parker <coreyparker012@gmail.com>
2021-07-31 17:51:00 -0400

    added routes for individual posts and basic commenting

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,19 +2,13 @@ const router = require('express').Router();
 const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', async (req, res) => {
-  res.render('homepage', {
-    logged_in: req.session.logged_in, 
-  });
-});
-
-router.get('/dashboard', async (req, res) => {
+router.get('/', withAuth, async (req, res) => {
   try {
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
     });
 
-    const projectData = await Post.findAll({
+    const postData = await Post.findAll({
       include: [
         {
           model: User,
@@ -23,7 +17,38 @@ router.get('/dashboard', async (req, res) => {
       ],
     });
 
-    const posts = projectData.map((post) => post.get({ plain: true }));
+    const posts = postData.map((xxx) => xxx.get({ plain: true }));
+    const users = userData.get({ plain: true });
+
+    res.render('homepage', { 
+      posts,
+      ...users, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+    });
+
+    const postData = await Post.findAll({
+      where: {
+        user_id: req.session.user_id
+      },
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const posts = postData.map((xxx) => xxx.get({ plain: true }));
     const users = userData.get({ plain: true });
 
     res.render('dashboard', { 
@@ -35,6 +60,47 @@ router.get('/dashboard', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+
+router.get('/posts/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+    
+    const posts = postData.get({ plain: true });
+
+    const commentData = await Comment.findAll({
+      where: {
+        post_id: req.params.id,
+      },
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+    
+    const comments = commentData.map((xxx) => xxx.get({ plain: true })
+    );
+
+    res.render('single-post', {
+      ...posts, 
+      comments,
+      logged_in: req.session.logged_in,
+      user_id: req.session.user_id, 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
+
 
 router.get('/login', async (req, res) => {
   // If the user is already logged in, redirect the request to another route
